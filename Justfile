feac86e83b17847b06ed6770eb043918e221aa53
Ethan Holz <ethan.holz@thoriumworks.com>
2025-01-11 16:38:43 -0700

    feat: switch to using inquire

--- Justfile
+++ Justfile
@@ -1,20 +1,36 @@
+cargo-version := `cargo metadata --no-deps --format-version=1 | jq -r '.packages[0].version'`
+
 build target:
     cargo zigbuild --target {{target}} --release -v
 
-release target:
+release target version:
     mkdir -p release
-    cp target/{{target}}/release/git-ce release/git-ce-{{target}} 
+    cp target/{{target}}/release/git-ce release/git-ce-{{target}}-{{version}}
 
-build-and-release target:
-    just build target={{target}}
-    just release target={{target}}
+build-and-release target version:
+    just build {{target}}
+    just release {{target}} {{version}}
 
-build-and-release-all:
-    just build-and-release "universal2-apple-darwin"
-    just build-and-release "x86_64-unknown-linux-musl"
-    just build-and-release "aarch64-unknown-linux-musl"
+build-release:
+    just build-all {{cargo-version}}
+
+build-all version:
+    just build-and-release "universal2-apple-darwin" {{version}}
+    just build-and-release "x86_64-unknown-linux-musl" {{version}}
+    just build-and-release "aarch64-unknown-linux-musl" {{version}}
+
+publish tag:
+    echo "Publishing tag {{tag}}"
+    gh release create {{tag}} --generate-notes release/*
 
 pre-release:
     cargo check --release --locked --all-targets
 
+sign:
+    op read "op://Private/minisign key password/password" | minisign -Sm release/*
 
+release-version:
+    just pre-release
+    just build-release
+    just sign
+    just publish "v{{cargo-version}}"
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::{fmt, io, process::Command};
 
-use clap::command;
-use console::Term;
-use dialoguer::{FuzzySelect, Input};
 use git2::{Repository, StatusOptions};
+use inquire::{Select, Text};
 
 #[derive(Default)]
 struct Commit {
@@ -14,6 +12,12 @@ struct Commit {
 
 impl fmt::Display for Commit {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.message.is_empty() {
+            match &self.breaking {
+                Some(_) => return write!(f, "{}!:", self.commit_type),
+                None => return write!(f, "{}:", self.commit_type),
+            }
+        }
         match &self.breaking {
             Some(breaking) => write!(
                 f,
@@ -28,7 +32,7 @@ impl fmt::Display for Commit {
 fn main() {
     let cwd = std::env::current_dir().unwrap();
     let cwd = cwd.to_str().unwrap();
-    let _ = command!().get_matches();
+    // let _ = command!().get_matches();
     let repo = match Repository::discover(cwd) {
         Ok(repo) => repo,
         Err(_err) => {
@@ -54,42 +58,21 @@ fn main() {
         }
         Err(err) => panic!("Error: {}", err),
     }
-    //
-    let selections = &[
+
+    let selections = vec![
         "feat", "fix", "chore", "ci", "docs", "style", "refactor", "perf", "test",
     ];
-    let term = Term::stdout();
     let mut commit = Commit::default();
-
-    let _ = ctrlc::set_handler(move || {
-        let term = Term::stdout();
-        let _ = term.show_cursor();
-        term.clear_to_end_of_screen().unwrap();
-        term.flush().unwrap();
-    });
-
-    let commit_types_result = FuzzySelect::new()
-        .with_prompt("Select a commit type")
-        .default(0)
-        .items(&selections[..])
-        .interact_on_opt(&term);
-    match commit_types_result {
-        Ok(commit_types) => {
-            match commit_types {
-                Some(commit_types) => commit.commit_type = selections[commit_types].to_string(),
-                None => return,
-            };
+    let ans = Select::new("Select a commit type", selections).prompt();
+    match ans {
+        Ok(choice) => {
+            commit.commit_type = choice.to_string();
         }
         Err(_) => return,
-    };
-    term.clear_last_lines(1).unwrap();
-    term.flush().unwrap();
+    }
 
     if parsed_scopes.len() == 1 {
-        let scope = Input::new()
-            .with_prompt("Scope")
-            .default("".to_string())
-            .interact_text();
+        let scope = Text::new("Scope").prompt();
         match scope {
             Ok(scope) => {
                 if !scope.is_empty() {
@@ -97,59 +80,35 @@ fn main() {
                 }
             }
             Err(_) => return,
-        };
-        term.clear_last_lines(1).unwrap();
-        term.flush().unwrap();
+        }
     } else {
-        let scope = FuzzySelect::new()
-            .with_prompt("Scope")
-            .default(0)
-            .items(&parsed_scopes[..])
-            .interact_on_opt(&term);
+        let scope = Select::new("Scope", parsed_scopes).prompt();
         match scope {
             Ok(scope) => {
-                match scope {
-                    Some(scope) => {
-                        commit.commit_type =
-                            format!("{}({})", commit.commit_type, parsed_scopes[scope]);
-                    }
-                    None => return,
-                };
+                if !scope.is_empty() {
+                    commit.commit_type = format!("{}({})", commit.commit_type, scope);
+                }
             }
             Err(_) => return,
-        };
-        term.clear_last_lines(1).unwrap();
-        term.flush().unwrap();
+        }
     }
 
-    let bc_result = Input::new()
-        .with_prompt("Breaking changes")
-        .default("".to_string())
-        .interact_text();
+    let bc_result = Text::new("Breaking changes").prompt();
     match bc_result {
         Ok(bc) => {
             commit.breaking = if bc.is_empty() { None } else { Some(bc) };
         }
-
         Err(_) => return,
-    };
+    }
 
-    term.clear_last_lines(1).unwrap();
-    term.flush().unwrap();
-
-    let message_result = Input::new()
-        .with_prompt(&commit.commit_type)
-        .interact_text();
-
-    match message_result {
+    let message_prompt = format!("{}", commit);
+    let message = Text::new(message_prompt.as_str()).prompt();
+    match message {
         Ok(message) => commit.message = message,
         Err(_) => return,
     }
-    let built: String = format!("{}", commit);
-    print!("{}", built);
-    term.clear_last_lines(1).unwrap();
-    term.flush().unwrap();
 
+    let built: String = format!("{}", commit);
     let _ = make_commit_shell(&built);
 }
 
